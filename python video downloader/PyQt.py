c2c3b7667b52712aa7e36f3842f45fcd4732364b
StickySputum <Bro.5n3k1t@yandex.ru>
2024-04-22 06:48:22 +0300

    new gui

--- python video downloader/PyQt.py
+++ python video downloader/PyQt.py
@@ -1,18 +1,20 @@
 from PySide2.QtWidgets import *
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QSettings
 import json
 from pytube import YouTube
 import threading
 import os
 from queue import Queue
 import sys
+import re
 
 class VideoDownloaderApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.selected_file_path = ""
-        self.save_directory = ""
+        self.settings = QSettings("VideoDownloaderApp", "settings")
+        self.save_directory = self.settings.value("save_directory", "", type=str)
         self.successful_downloads = 0
         self.total_videos = 0
         self.lock = threading.Lock()
@@ -52,8 +54,10 @@ class VideoDownloaderApp(QMainWindow):
             self.output_text.append("Selected File: " + os.path.basename(self.selected_file_path))
 
     def select_save_directory(self):
-        self.save_directory = QFileDialog.getExistingDirectory(self, "Choose Save Directory")
-        if self.save_directory:
+        new_save_directory = QFileDialog.getExistingDirectory(self, "Choose Save Directory", self.save_directory)
+        if new_save_directory:
+            self.save_directory = new_save_directory
+            self.settings.setValue("save_directory", self.save_directory)
             self.output_text.append(f"Selected Save Directory: {self.save_directory}")
 
     def download_video(self):
@@ -62,9 +66,13 @@ class VideoDownloaderApp(QMainWindow):
             try:
                 yt = YouTube(link)
                 stream = yt.streams.get_highest_resolution()
-                title = ''.join(c for c in yt.title if c.isalnum() or c in [' ', '.', '_', '-'])
-                save_path = os.path.join(save_dir, title + '.mp4')
-                stream.download(output_path=save_dir, filename=title + '.mp4')
+                title = yt.title
+
+                # Удаляем или заменяем недопустимые символы для Windows в названии файла
+                title_clean = re.sub(r'[<>:"/\\|?*]', '_', title)
+                save_path = os.path.join(save_dir, f"{title_clean}" + '.mp4')
+                stream.download(output_path=save_dir, filename=title_clean + '.mp4')
+
                 with self.lock:
                     self.successful_downloads += 1
                     self.update_progress_bar()
@@ -73,6 +81,7 @@ class VideoDownloaderApp(QMainWindow):
                         self.output_text.append("All videos have been downloaded!")
             except Exception as e:
                 self.output_text.append(f"Error downloading video from link {link}: {e}")
+                self.successful_downloads += 1
             finally:
                 self.download_queue.task_done()
 
@@ -104,7 +113,7 @@ class VideoDownloaderApp(QMainWindow):
             self.download_queue.put((link, save_dir))
 
         # Start download threads
-        for _ in range(min(3, len(video_links))):
+        for _ in range(min(1, len(video_links))):
             t = threading.Thread(target=self.download_video)
             t.daemon = True
             t.start()
