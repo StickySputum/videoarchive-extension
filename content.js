ba022f80382d893cd2a1d2b77ce6e6cb2c48fef5
StickySputum <Bro.5n3k1t@yandex.ru>
2024-07-26 06:08:48 +0300

    youtube video check

--- content.js
+++ content.js
@@ -1,16 +1,20 @@
-// Contents of content.js
-var currentUrl = window.location.href;
+if (window.location.href.includes("/watch?v=")) {
+  // Contents of content.js
+  var currentUrl = window.location.href;
 
-chrome.storage.local.get('savedUrls', function(data) {
-    var savedUrls = data.savedUrls || [];
+  chrome.storage.local.get('savedUrls', function(data) {
+      var savedUrls = data.savedUrls || [];
 
-    // Добавляем новую ссылку в массив
-    savedUrls.push(currentUrl);
+      // Добавляем новую ссылку в массив
+      savedUrls.push(currentUrl);
 
-    // Сохраняем обновленный массив в локальное хранилище
-    chrome.storage.local.set({ 'savedUrls': savedUrls }, function() {
-        console.log('Новая ссылка добавлена в локальное хранилище: ', currentUrl);
-        // Уведомляем всплывающее окно об обновлении
-        chrome.runtime.sendMessage({ action: "updatePopup", url: currentUrl });
-    });
-});
+      // Сохраняем обновленный массив в локальное хранилище
+      chrome.storage.local.set({ 'savedUrls': savedUrls }, function() {
+          console.log('Новая ссылка добавлена в локальное хранилище: ', currentUrl);
+          // Уведомляем всплывающее окно об обновлении
+          chrome.runtime.sendMessage({ action: "updatePopup", url: currentUrl });
+      });
+  });
+} else {
+  alert("Это не видео")
+}
